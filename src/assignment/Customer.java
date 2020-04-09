c9aa462705d83007ad7264e8070956ddae54271f
bhanu345-cmd <bhanuprathusha@gmail.com>
2020-04-09 16:25:22 +0530

    by bhanu

---
+++ src/assignment/Customer.java
@@ -0,0 +1,57 @@
+package assignment;
+
+import java.util.Date;
+
+public class Customer {
+	private int id;
+	private String name;
+	private String email;
+	private String mobile;
+	private Date dob;
+	private Address address;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + ", email=" + email + ", mobile=" + mobile + ", dob=" + dob
+				+ ", address=" + address + "]";
+	}
+	
+	
+	
+}
